f779ce9f1d8a0fb78971cd16ce68ced9bd4f4d56
Danylo Kanievskyi <danylokaniev@gmail.com>
2020-09-16 23:04:13 +0300

    Updates

---
+++ precache-manifest.155d277405a2b0a4257ecc72bdf26158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154b8968ffe3d3e4b7a9fc9d63bd49c4",
+    "url": "/adkernel-test-task/index.html"
+  },
+  {
+    "revision": "df351feb6136635b21c1",
+    "url": "/adkernel-test-task/static/css/main.3048918a.chunk.css"
+  },
+  {
+    "revision": "1ebcdcf3f5935b5f9b89",
+    "url": "/adkernel-test-task/static/js/2.673e5102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adkernel-test-task/static/js/2.673e5102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df351feb6136635b21c1",
+    "url": "/adkernel-test-task/static/js/main.1273e6aa.chunk.js"
+  },
+  {
+    "revision": "9f5b6eb348add68b0a8a",
+    "url": "/adkernel-test-task/static/js/runtime-main.2ab38f4f.js"
+  }
+]);
